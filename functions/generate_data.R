2238c1d1fa9474ed08182f82f5e028d3faf41a13
Andrew Mertens <amertens@berkeley.edu>
2023-06-22 07:21:27 -0700

    clean output

--- functions/generate_data.R
+++ functions/generate_data.R
@@ -11,9 +11,10 @@ generate_data <- function(cc, seed=12345, reps=sim_reps, n=dataset_N, N_time=10)
   for(i in 1:reps){
     cat("\ni: ",i,"\n")
     
-
+    d <- sim(u,n)
+    
       d<- clean_sim_data(d, N_time = 12)      
-
+    
     
     #add identifiers
     d$pnr <- 1:nrow(d)
