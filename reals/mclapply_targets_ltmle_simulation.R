474b28ee578bf5011b6ad1d2f80e2b34e362eefc
andrew.mertens <andrew.mertens3@gmail.com>
2023-08-09 08:57:43 -0700

    Null res

--- reals/mclapply_targets_ltmle_simulation.R
+++ reals/mclapply_targets_ltmle_simulation.R
@@ -1,28 +1,36 @@
 
-n_bootstrap_samples=2
-time=2
-n_cores=90
-estimator="undersmoothed ridge markov"
-seeds=seeds1[1:2]
-library="glmnet"
-SL.Control=list(selector="undersmooth",alpha=0)
-gbounds=c(0.01,1)
+#An_bootstrap_samples=2
+# time=2
+# n_cores=90
+# estimator="undersmoothed ridge markov"
+# seeds=seeds1[1:2]
+# library="glmnet"
+# SL.Control=list(selector="undersmooth",alpha=0)
+# gbounds=c(0.01,1)
+# 
+# null_sim=TRUE
+# n_cores=22
+# estimator="glm"
+# seeds=seeds_null
+# library="glm"
+# n_df=100000
+# n_bootstrap_samples=0
 
 mclapply_targets_ltmle_simulation <- function(seeds, n_df=100000, n_cores=50,
                                               library="glm",
                                               SL.Control=NULL,
                                               time=10,
                                               gbounds=c(0.01,1),
+                                              null_sim=FALSE,
                                               n_bootstrap_samples=0,
                                               Markov_variables=NULL,
                                               estimator=""){
-  #browser()
-  start=Sys.time()
+
 
   
   cl <- makeCluster(n_cores)
   clusterExport(cl, c("seeds","run_targets_ltmle_simulation",  "library",
-                      "SL.Control",
+                      "SL.Control","null_sim",
                       "n_bootstrap_samples",
                       "Markov_variables",
                       "gbounds",
@@ -35,6 +43,7 @@ mclapply_targets_ltmle_simulation <- function(seeds, n_df=100000, n_cores=50,
 
   res=parLapply(cl=cl,seeds, function(z) run_targets_ltmle_simulation(seed=z,
                                                             library=library,
+                                                            null_sim=null_sim,
                                                             SL.Control=SL.Control,
                                                             n_bootstrap_samples=n_bootstrap_samples,
                                                             Markov_variables=Markov_variables,
